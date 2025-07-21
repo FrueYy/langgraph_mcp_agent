587b4b8d7eba327cd786b556e6474f4a9ab35ac9
FrueYy <563845034@qq.com>
2025-07-21 14:35:22 +0800

    add tianfu resources

--- core/mcp_server/resources/resource_server.py
+++ core/mcp_server/resources/resource_server.py
@@ -1,9 +1,8 @@
 import os
-import fitz  # PyMuPDF 用于读取 PDF 文件
+import fitz  
 from mcp.server.fastmcp import FastMCP
 
-# 你的资源文件夹路径（相对路径或绝对路径均可）
-
+#本地天洑资源服务器
 mcp = FastMCP(name="LocalResourceServer")
 RESOURCE_DIR = "/mnt/e/project/langgraph_mcp_agent/core/mcp_server/resources/njtf_resources/"
 for filename in os.listdir(RESOURCE_DIR):
@@ -29,5 +28,4 @@ for filename in os.listdir(RESOURCE_DIR):
 
 
 if __name__ == "__main__":
-    # 启动 MCP 服务器，使用 stdio 通信
     mcp.run(transport="stdio")
